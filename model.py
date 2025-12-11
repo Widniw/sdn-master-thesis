8703f9a0c94fcf030a7e243a7dd4f8b235d0cd14
Krzysztof Wdowczyk <krzys.wdowczyk@gmail.com>
2025-12-12 00:22:03 +0100

    Add first delay calculation

--- model.py
+++ model.py
@@ -5,13 +5,26 @@ SERVICE_RATE = 3000
 
 G = nx.Graph()
 
+flows_info = {1: {"path": (1),
+                  "packet_rate": 2500}}
+
 switches_info = {1: {"service_rate": SERVICE_RATE,
-                "queue_capacity": QUEUE_CAPACITY}
+                "queue_capacity": QUEUE_CAPACITY,
+                "aggr_arrival_rate": 2999}
             }
 
 switches = [Switch(**switches_info[i]) for i in switches_info]
 
-G.add_nodes_from(switches)
+for i, switch in enumerate(switches):
+    G.add_node(i, data = switch)
 
-print(list(G.nodes))
+
+
+G.nodes[0]['data'].aggr_arrival_rate = flows_info[1]["packet_rate"]
+print(f"Switch 1 Aggr arrival rate: {G.nodes[0]['data'].aggr_arrival_rate}")
+print(f"Switch 1 Packet Loss Probability: {G.nodes[0]['data'].packet_loss_probabilty}")
+print(f"Switch 1 Expected Queue Occupation: {G.nodes[0]['data'].exp_queue_occupation}")
+print(f"Switch 1 Expected Delay [s]: {G.nodes[0]['data'].exp_delay}")
+
+
 
--- switch.py
+++ switch.py
@@ -1,5 +1,5 @@
 class Switch:
-    def __init__(self, service_rate, queue_capacity, queue_length = 0, aggr_arrival_rate = 0):
+    def __init__(self, service_rate, queue_capacity, queue_length = 0, aggr_arrival_rate = 1):
         self.service_rate = service_rate
         self.queue_capacity = queue_capacity
         self.queue_length = queue_length
@@ -10,3 +10,6 @@ class Switch:
         self.exp_queue_occupation = (self.traffic_intensity/(1 - self.traffic_intensity)) - \
                                     ((self.queue_capacity + 1) * self.traffic_intensity**(self.queue_capacity + 1)) \
                                     /(1 - self.traffic_intensity**(self.queue_capacity + 1))
+        self.exp_delay = self.exp_queue_occupation / (self.aggr_arrival_rate * (1 - self.packet_loss_probabilty))
+
+        
