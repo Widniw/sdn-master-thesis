fbde2146bca6d425fd7b087431a163dfb047b577
Krzysztof <412158@student.agh.edu.pl>
2026-03-15 15:00:46 +0100

    Moved traffic_leaving_mm1k to utils.py

--- utils.py
+++ utils.py
@@ -28,3 +28,38 @@ def json2networkx(json_data):
 
 
     return G
+
+def traffic_leaving_mm1k(incoming_flows, service_rate, queue_capacity):
+    aggregated_traffic = 0
+    for edge_flows in incoming_flows.values():
+        aggregated_traffic += sum(edge_flows.values())
+
+    # Jeśli nie ma ruchu, zwracamy puste/zerowe flows bez liczenia
+    if aggregated_traffic == 0:
+        return incoming_flows
+
+    # 2. Obliczenie obciążenia (rho)
+    ro = aggregated_traffic / service_rate
+
+    if ro == 1:
+        ro = 0.999
+
+    # Unikanie overflow
+    if ro < 1.0:
+        leaving_probability = (1 - ro**queue_capacity) / (1 - ro**(queue_capacity + 1))
+            
+    else:
+        numerator = (1.0 / ro) - (ro ** -(queue_capacity + 1))
+        denominator = 1.0 - (ro ** -(queue_capacity + 1))
+        leaving_probability = numerator / denominator    
+    
+    outgoing_flows = {}
+    
+    for edge, flows_dict in incoming_flows.items():
+        new_flows_dict = {}
+        for flow_id, traffic in flows_dict.items():
+            new_flows_dict[flow_id] = traffic * leaving_probability
+    
+        outgoing_flows[edge] = new_flows_dict
+
+    return outgoing_flows
