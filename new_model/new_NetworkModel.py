2178ae7e30f138f23da447ad416a3305d1d92817
Krzysztof <412158@student.agh.edu.pl>
2026-03-22 14:29:14 +0100

    Add new network model where each leaving port gets its own queue

---
+++ new_model/new_NetworkModel.py
@@ -0,0 +1,243 @@
+from switch import Switch
+import numpy as np
+from pathlib import Path
+from new_model.new_model_utils import json2networkx
+import random
+import networkx as nx
+
+class NetworkModel:
+    def __init__(self, G):
+        self.G = G
+        self.switches = [(node, attr) for node, attr in self.G.nodes(data=True) if isinstance(attr.get('data'), Switch)]
+        self.no_of_nodes = len(self.G.nodes)
+        self.no_of_switches = len(self.switches)
+        self.edges = [edge for edge in self.G.edges()]
+        self.no_of_edges = len(self.edges)
+        self.mu_max = 3000
+
+        self.node_to_index = {}
+        for i, node in enumerate(self.G.nodes()):
+            self.node_to_index[node] = i
+    
+    def calculate_measurements(self, flows_traffic, flows_paths):
+        self.flows_traffic = flows_traffic
+        self.flows_paths = flows_paths
+
+        self.ziped_flow_paths = {}
+
+        for flow, path in self.flows_paths.items():
+            zipped_path = []
+            for u, v in zip(path, path[1:]):
+                zipped_path.append((u,v))
+            self.ziped_flow_paths[flow] = zipped_path
+        
+        no_of_flows = len(self.flows_paths)
+        self.flow_to_index = {}
+        for i, flow in enumerate(self.flows_traffic):
+            self.flow_to_index[flow] = i
+        
+        self.full_AVTM_matrix = np.zeros((self.no_of_nodes, self.no_of_nodes, no_of_flows), dtype=np.float32)
+
+        for flow_name, traffic in self.flows_traffic.items():
+            path = self.ziped_flow_paths[flow_name]
+            flow_idx = self.flow_to_index[flow_name]
+
+            for u, v in path:
+                u_idx = self.node_to_index[u]
+                v_idx = self.node_to_index[v]
+
+                self.full_AVTM_matrix[u_idx, v_idx, flow_idx] = traffic
+        
+        for i in range(18):
+            temp_full_AVTM_matrix = self.full_AVTM_matrix
+
+            switch_leaving_prob = {}
+
+            for switch, attributes in self.switches:
+                v_idx = self.node_to_index[switch]
+
+                total_incoming = self.full_AVTM_matrix[:, v_idx, :].sum()
+                # if switch == "4_3":
+                #     print(f"iteration {i}: total incoming switch {switch}: {total_incoming}")
+
+                service_rate = attributes["data"].service_rate
+                queue_capacity = attributes["data"].queue_capacity
+                ro = total_incoming / service_rate
+
+                if ro == 1:
+                    ro = 0.999
+
+                if ro < 1.0:
+                    leaving_probability = (1 - ro**queue_capacity) / (1 - ro**(queue_capacity + 1))
+                        
+                else:
+                    numerator = (1.0 / ro) - (ro ** -(queue_capacity + 1))
+                    denominator = 1.0 - (ro ** -(queue_capacity + 1))
+                    leaving_probability = numerator / denominator
+                
+                switch_leaving_prob[switch] = leaving_probability
+                # if switch == "4_3":
+                #     print(f"Iteration {i}: switch {switch} leaving prop: {leaving_probability}")
+            
+            for flow, zipped_path in self.ziped_flow_paths.items():
+                last_index = len(zipped_path) - 1
+
+                for (u, v) in zipped_path:
+                    curr_index = zipped_path.index((u, v))
+
+                    if curr_index == last_index:
+                        continue
+
+                    if v not in [s[0] for s in self.switches]:
+                        continue
+                    
+                    u_idx = self.node_to_index[u]
+                    v_idx = self.node_to_index[v]
+                    flow_idx = self.flow_to_index[flow]
+
+                    traffic = self.full_AVTM_matrix[u_idx, v_idx, flow_idx]
+
+                    outgoing_traffic = traffic * switch_leaving_prob[v]
+
+                    next_edge_index = curr_index + 1
+
+                    next_u, next_v = zipped_path[next_edge_index]
+                    next_u_idx = self.node_to_index[next_u]
+                    next_v_idx = self.node_to_index[next_v]
+
+                    temp_full_AVTM_matrix[next_u_idx, next_v_idx, flow_idx] = outgoing_traffic
+                
+            
+            self.full_AVTM_matrix = temp_full_AVTM_matrix
+
+        delay_at_switch = {}
+
+        for switch, attributes in self.switches:
+                v_idx = self.node_to_index[switch]
+
+                total_incoming = self.full_AVTM_matrix[:, v_idx, :].sum()
+                # if switch == "4_3":
+                #     print(f"total incoming for switch {switch}: {total_incoming}")
+
+                if total_incoming == 0:
+                    delay_at_switch[switch] = 0
+                    continue
+
+                total_outgoing = self.full_AVTM_matrix[v_idx, :, :].sum()
+                # if switch == "4_3":
+                #     print(f"total outgoing for switch {switch}: {total_outgoing}")
+
+
+                service_rate = attributes["data"].service_rate
+                queue_capacity = attributes["data"].queue_capacity
+                ro = total_incoming / service_rate        
+
+                if ro == 1:
+                    L_system = queue_capacity / total_outgoing
+                elif ro < 1.0:
+                    term1 = ro / (1 - ro)
+                    term2 = ((queue_capacity + 1) * (ro ** (queue_capacity + 1))) / (1 - (ro ** (queue_capacity + 1)))    
+                else: # ro > 1
+                    term1 = ro / (1 - ro)
+                    term2 = (queue_capacity + 1) / ((ro ** -(queue_capacity + 1)) - 1)
+                
+                L_system = term1 - term2
+
+                exp_delay_at_switch = L_system / total_outgoing
+                print(f"exp_delay_at switch {switch}: {exp_delay_at_switch}")
+
+                delay_at_switch[switch] = exp_delay_at_switch                    
+
+        delays = np.zeros(no_of_flows)
+        total_incoming_network = 0
+
+        for i, ((src, dst), traffic) in enumerate(self.flows_traffic.items()):
+            total_incoming_network += traffic
+
+            path = self.flows_paths[(src, dst)]
+
+            total_delay = 0
+
+            for node in path[1:-1]:
+                if node in self.switches:
+                    total_delay += delay_at_switch[node]
+
+            delays[i] = total_delay
+
+        avg_delay = np.mean(delays)
+
+        total_packet_loss = 0
+
+        for switch, attributes in self.switches:
+            v_idx = self.node_to_index[switch]
+            total_incoming = self.full_AVTM_matrix[:, v_idx, :].sum()
+            total_outgoing = self.full_AVTM_matrix[v_idx, :, :].sum()
+
+            packet_loss = total_incoming - total_outgoing
+            total_packet_loss += packet_loss
+    
+        switch_AVTM_matrix = np.zeros((self.no_of_switches, self.no_of_switches), dtype=np.float32)
+
+        # for src_switch, attributes in self.switches:
+        #     for dst_switch, attributes in self.switches:
+
+
+        #         raw_traffic = self.full_AVTM_matrix[u_idx, v_idx, :].sum()
+
+        #         # 2. IMPLEMENT EQUATION 10: Normalize and bound to [0, 1]
+        #         normalized_traffic = min(1.0, raw_traffic / self.mu_max)
+
+        #         # 3. Store in the final ATVM matrix
+        #         switch_AVTM_matrix[int(src_switch)][int(dst_switch)] = normalized_traffic
+
+        return avg_delay, total_packet_loss, switch_AVTM_matrix
+                
+if __name__ == '__main__':
+    base_dir = Path(__file__).resolve().parent.parent
+    topology_path = base_dir / "topologies" / "mesh5x5.json"
+
+    G = json2networkx(topology_path)
+    model = NetworkModel(G)
+
+    alpha = 0.9
+    mu_max = 3000
+    K_max = 10000
+    max_hops = 25
+    
+    flows_traffic = {}
+    no_of_flows = 150
+    total_incoming_network = 0
+
+    for flow in range(no_of_flows):
+        edge_switches = (0, 1, 2, 3, 4, 5, 10, 15, 20, 21, 22, 23, 24, 19, 14, 9)
+        random_hosts = random.sample(edge_switches, 2)
+        random_traffic_rate = random.uniform(10, 300)
+        total_incoming_network += random_traffic_rate
+        if (f"10.0.1.{random_hosts[0]}",f"10.0.1.{random_hosts[1]}") not in flows_traffic:
+            flows_traffic[(f"10.0.1.{random_hosts[0]}",f"10.0.1.{random_hosts[1]}")] = 0
+        flows_traffic[(f"10.0.1.{random_hosts[0]}",f"10.0.1.{random_hosts[1]}")] += random_traffic_rate
+
+    # flows_traffic = {("10.0.1.0", "10.0.1.1"): 3,
+    #          ("10.0.1.1", "10.0.1.0"): 2} 
+
+    all_paths = dict(nx.all_pairs_dijkstra_path(model.G, weight="weight"))
+
+    flows_paths = {}
+    for (src, dst), traffic in flows_traffic.items():
+        path = all_paths[src][dst] 
+        flows_paths[(src, dst)] = path
+    
+    avg_delay, total_packet_loss, switch_AVTM_matrix = model.calculate_measurements(flows_traffic, flows_paths)
+    print(f"{avg_delay =}")
+    print(f"{total_packet_loss = }")
+    print(f"{switch_AVTM_matrix = }")
+
+    max_possible_delay = max_hops * (K_max / mu_max)
+    
+    # rd(t) and rp(t) formulas [cite: 296, 330]
+    r_d = 1.0 - min(avg_delay / max_possible_delay, 1.0)
+    r_p = 1.0 - min(total_packet_loss / total_incoming_network, 1.0) if total_incoming_network > 0 else 1.0
+    
+    # Total Reward R(st, at) 
+    reward = alpha * r_d + (1 - alpha) * r_p
+    print(f"{reward = }")
